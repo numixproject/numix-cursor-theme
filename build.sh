2e02618d5d94a3df813504dc59e90ee523699090
Joshua Fogg <joshua.h.fogg@gmail.com>
2021-10-02 14:02:37 +0100

    Better debugging Improved the formatting of the debug output

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# colourful messages for debugging
+function fail { echo -e "\033[0;31mFAILED:\033[0m $1"; exit 1; }  # red, fatal
+function warn { echo -e "\033[0;33mFAILED:\033[0m $1"; }          # orange, issue
+function success { echo -e "\033[0;32mDONE!\033[0m"; }            # green, success
+
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
 RAWSVGS_LIGHT="src/light/svgs"
@@ -8,24 +14,20 @@ INDEX_DARK="src/dark/cursor.theme"
 ALIASES="src/cursorList"
 
 
-echo -ne "Checking Requirements...\\r"
-if [ ! -f $INDEX_LIGHT ] ; then
-    echo -e "\\nFAIL: '$INDEX_LIGHT' missing"
-    exit 1
-elif [ ! -f $INDEX_LIGHT ] ; then
-    echo -e "\\nFAIL: '$INDEX_DARK' missing"
-    exit 1
-elif ! type "inkscape" > /dev/null ; then
-    echo -e "\\nFAIL: inkscape must be installed"
-    exit 1
-elif ! type "xcursorgen" > /dev/null ; then
-    echo -e "\\nFAIL: xcursorgen must be installed"
-    exit 1
+echo -ne "Checking Requirements..."
+if [ ! -f $INDEX_LIGHT ]; then
+    fail "'$INDEX_LIGHT' missing"
+elif [ ! -f $INDEX_DARK ]; then
+    fail "'$INDEX_DARK' missing"
+elif ! type "inkscape" > /dev/null; then
+    fail "Inkscape v1.1+ must be installed"
+elif ! type "xcursorgen" > /dev/null; then
+    fail "xcursorgen must be installed"
 fi
-echo -e "Checking Requirements... DONE"
+success
 
 
-echo -ne "Making Folders... $BASENAME\\r"
+echo -ne "Making Folders... $BASENAME"
 DIR2X_LIGHT="build/light/x2"
 DIR1X_LIGHT="build/light/x1"
 DIR2X_DARK="build/dark/x2"
@@ -40,11 +42,11 @@ mkdir -p "$DIR2X_LIGHT"
 mkdir -p "$DIR1X_LIGHT"
 mkdir -p "$OUTPUT_LIGHT/cursors"
 mkdir -p "$OUTPUT_DARK/cursors"
-echo 'Making Folders... DONE';
+success
 
 
 # export SVG as PNG, ran using `render $FILENAME $INPUTDIR $OUTPUTDIR $SIZE`
-render() { inkscape "$2"/"$1".svg -o "$3"/"$1".png -w "$4"; }
+function render { inkscape "$2"/"$1".svg -o "$3"/"$1".png -w "$4" 2> /dev/null; }
 
 
 for CUR in src/config/*.cursor; do
@@ -59,12 +61,11 @@ for CUR in src/config/*.cursor; do
     render "$BASENAME" "$RAWSVGS_DARK" "$DIR1X_DARK" 32
     render "$BASENAME" "$RAWSVGS_DARK" "$DIR2X_DARK" 64
 done
-echo -e "\\033[0KGenerating simple cursor pixmaps... DONE"
+echo -ne "\\033[0KGenerating simple cursor pixmaps... "; success
 
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
-do
-    echo -ne "\\033[0KGenerating animated cursor pixmaps... $i / 24 \\r"
+for i in {01..24}; do
+    echo -ne "\\033[0KGenerating animated cursor pixmaps... $i/24 \\r"
 
     render "progress-$i" "$RAWSVGS_LIGHT" "$DIR1X_LIGHT" 32
     render "progress-$i" "$RAWSVGS_LIGHT" "$DIR2X_LIGHT" 64
@@ -76,51 +77,50 @@ do
     render "wait-$i" "$RAWSVGS_DARK" "$DIR1X_DARK" 32
     render "wait-$i" "$RAWSVGS_DARK" "$DIR2X_DARK" 64
 done
-echo -e "\\033[0KGenerating animated cursor pixmaps... DONE"
+echo -ne "\\033[0KGenerating animated cursor pixmaps... "; success
 
 
-echo -ne "Generating cursor theme...\\r"
+echo -ne "Generating cursor theme... "
 for CUR in src/config/*.cursor; do
     BASENAME=$CUR
     BASENAME=${BASENAME##*/}
     BASENAME=${BASENAME%.*}
 
     if ! ERR="$( xcursorgen -p build/light "$CUR" "$OUTPUT_LIGHT/cursors/$BASENAME" 2>&1 )"; then
-        echo "FAIL: $CUR $ERR"
+        warn "$CUR $ERR"
     fi
 
     if ! ERR="$( xcursorgen -p build/dark "$CUR" "$OUTPUT_DARK/cursors/$BASENAME" 2>&1 )"; then
-        echo "FAIL: $CUR $ERR"
+        warn "$CUR $ERR"
     fi
 done
-echo -e "Generating cursor theme... DONE"
+success
 
 
-echo -ne "Generating shortcuts...\\r"
-while read -r ALIAS ; do
+echo -ne "Generating shortcuts... "
+while read -r ALIAS; do
     FROM=${ALIAS% *}
     TO=${ALIAS#* }
-    if [ -e "$OUTPUT_LIGHT/cursors/$FROM" ] ; then
+    if [ -e "$OUTPUT_LIGHT/cursors/$FROM" ]; then
         continue
     fi
     ln -sf "$TO" "$OUTPUT_LIGHT/cursors/$FROM"
 
-    if [ -e "$OUTPUT_DARK/cursors/$FROM" ] ; then
+    if [ -e "$OUTPUT_DARK/cursors/$FROM" ]; then
         continue
     fi
     ln -sf "$TO" "$OUTPUT_DARK/cursors/$FROM"
 done < $ALIASES
-echo -e "\\033[0KGenerating shortcuts... DONE"
+success
 
 
-echo -ne "Copying Theme Index...\\r"
-    if ! [ -e "$OUTPUT_LIGHT/$INDEX_LIGHT" ] ; then
+echo -ne "Copying Theme Index... "
+    if ! [ -e "$OUTPUT_LIGHT/$INDEX_LIGHT" ]; then
         cp $INDEX_LIGHT "$OUTPUT_LIGHT/cursor.theme"
     fi
-    if ! [ -e "$OUTPUT_DARK/$INDEX_DARK" ] ; then
+    if ! [ -e "$OUTPUT_DARK/$INDEX_DARK" ]; then
         cp $INDEX_DARK "$OUTPUT_DARK/cursor.theme"
     fi
-echo -e "\\033[0KCopying Theme Index... DONE"
+success
 
-
-echo "COMPLETE!"
+echo "Build complete!"
